91ceb3d5649a84f76b2e28091ff889c7cfe67703
super-archive-1419 <sophzsul@gmail.com>
2020-01-20 04:47:02 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -30,7 +30,7 @@ function accessElementInArray(array, index) {
  function removeElementFromBeginningOfArray(array){
    return array.slice(1);
  }
-  
+
  function destructivelyRemoveElementFromEndOfArray(array){
    array.pop();
    return array;

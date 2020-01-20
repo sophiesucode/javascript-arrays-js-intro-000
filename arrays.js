836d8861be3756821027d59617606c176ded0a64
super-archive-1419 <sophzsul@gmail.com>
2020-01-20 03:09:58 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,8 +2,6 @@ var chocolateBars =['snickers','hundred grand','kitkat', 'skittles'];
 
 //return new array and not alter original one......use the spread operator to create a new array in place, rather than modifying the original one
 //expect(addElementToBeginningOfArray([1], 'foo')).to.eql(['foo', 1])
-const oldArray=[1]
-const addElementToBeginningOfArray(oldArray,'foo'){
-  return ['foo',...oldArray];
-};
 //const moreCats = ["foo", ...array]
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array];

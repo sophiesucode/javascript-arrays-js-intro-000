c82afe1c6e03631fa435d0721128036b4962597f
super-archive-1419 <sophzsul@gmail.com>
2020-01-20 04:17:50 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -21,6 +21,6 @@ function destructivelyAddElementToEndOfArray(array, foo){
   return array;
 }
 const newArray = [1, 2, 3]
-function accessElementInArray(array, 2){
- return array[2];
+function accessElementInArray(newArray, 2){
+ return newArray[2];
  }
